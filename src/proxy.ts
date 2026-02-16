aea9340b1b77ee25afe210b7dab6da3a9e3c32f5
tharindu <tharindu@epitoni.com>
2026-02-16 15:53:50 +0530

    fix:proxy(2)

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,32 +1,32 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const PUBLIC_ROUTES = ['/login', '/register'];
+const PUBLIC_ROUTES = ["/login", "/register"];
 
 export function proxy(request: NextRequest) {
-  const token = request.cookies.get('access_token')?.value;
-  console.log("token found from cookie:",token)
+  const token = request.cookies.get("access_token")?.value;
+  console.log("token found from cookie:", token);
   const { pathname } = request.nextUrl;
 
-  const isPublicRoute = PUBLIC_ROUTES.some(route =>
+  const isPublicRoute = PUBLIC_ROUTES.some((route) =>
     pathname.startsWith(route)
   );
-
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
   // Not logged in → block private routes
-  // if (!token && !isPublicRoute) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  //   if (!token && !isPublicRoute) {
+  //     return NextResponse.redirect(new URL('/login', request.url));
+  //   }
 
-  // // Logged in → block login page
-  // if (token && isPublicRoute) {
-  //   return NextResponse.redirect(new URL('/dashboard', request.url));
-  // }
+  // //   Logged in → block login page
+  //   if (token && isPublicRoute) {
+  //     return NextResponse.redirect(new URL('/dashboard', request.url));
+  //   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
